05b647ded0ebce589ac2ecac917a079b7700dd5b
PravallikaDeepu <mpravallika1606@gmail.com>
2026-06-26 18:36:51 +0530

    added styles

--- src/components/pages/UserDashboard.js
+++ src/components/pages/UserDashboard.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function UserDashboard() {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/user/login");
+  };
+
   return (
-    <div>
-      <h1>User Dashboard</h1>
+   <div className="dashboard-header">
+  <div className="row h-100 align-items-center">
+    <div className="col-3"></div>
+
+    <div className="col-6 text-center">
+      <h1 className="welcome-text">
+        Welcome, {user?.name} 👋
+      </h1>
     </div>
-  )
+
+    <div className="col-3 text-end pe-4">
+      <button
+        className="btn btn-light rounded-pill logout-btn"
+        onClick={handleLogout}
+      >
+        <i className="bi bi-box-arrow-right me-2"></i>
+        Logout
+      </button>
+    </div>
+  </div>
+</div>
+  );
 }
